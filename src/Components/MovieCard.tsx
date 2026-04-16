79dba78d2304f5e7b138006b8faf11ebbe17ffcb
anil <4nonymouscode@gmail.com>
2026-04-16 18:16:53 +0530

    update

--- src/Components/MovieCard.tsx
+++ src/Components/MovieCard.tsx
@@ -3,9 +3,9 @@ const MovieCard = ({myData}:{myData:any}) => {
     const {title,body,id}=myData
   return (
     <div className="card">
-      <div className="card-info">
-        <p className="card-id">{id}</p>
-        <p>{body.substr(0,150)}</p>
+      <div className="card-info" >
+        <p className="bg-white w-[30px] h-[30px] rounded-full flex justify-center item-center sm: ">{id}</p>
+        <p className=" text-sm sm:text-base lg:text-lg xl:text-xl">{body.substr(0,150)}</p>
         <h2>{title.substr(0,15)}</h2>
       </div>
     </div>
